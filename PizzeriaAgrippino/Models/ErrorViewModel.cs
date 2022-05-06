30ea64e9d7abc54e8ff18a486085a452ffc73c02
Agrippino <agrippinorussiello@outlook.it>
2022-05-06 14:51:03 +0200

    Add project files.

---
+++ PizzeriaAgrippino/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PizzeriaAgrippino.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
